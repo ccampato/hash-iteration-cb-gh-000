9f97395891ecb328d8c2168b53c7c32fe38bcb27
Caio Sanches <ccampato@gmail.com>
2018-04-24 05:36:09 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,12 +1,10 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, velue|
-    puts "Happy Birthday #{name}! You are now #{value}"
+    puts "Happy Birthday #{name}! You are now #{value}!"
 end
-
-
